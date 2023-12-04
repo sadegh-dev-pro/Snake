dae66f8e816194a2f2295d552400269d3019f988
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 03:26:57 +0330

    initial commit

---
+++ app/src/main/java/com/sadegh/snake/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.sadegh.snake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sadegh.snake.ui.theme.SnakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SnakeTheme {
+            }
+        }
+    }
+}
