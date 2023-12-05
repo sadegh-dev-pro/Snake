18514b4bb5a584b103e57745dcf4f5629458cc1f
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 06:26:12 +0330

    call ControlSection function in MainScreen function and put on and GameBoarder function in column

--- app/src/main/java/com/sadegh/snake/presentation/game_screen/MainScreen.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.sadegh.snake.presentation.game_screen
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,10 +9,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sadegh.snake.presentation.game_screen.components.ControlSection
 import com.sadegh.snake.presentation.game_screen.components.GameBoard
 
 @Composable
@@ -22,12 +25,28 @@ fun MainScreen() {
 
         val snakePixelsPosition by viewModel.snakePixelPositions.collectAsState()
 
-        GameBoard(
-            modifier = Modifier
-                .padding(start = 25.dp, end = 25.dp, top = 25.dp),
-            numberOfSquaresInHeight =21,
-            numberOfSquaresInWidth =12,
-            snakePixelPositions =snakePixelsPosition
-        )
+        Column(modifier = Modifier.fillMaxSize()) {
+
+            val gameBoardModifier = remember {
+                Modifier
+                    .padding(start = 25.dp, end = 25.dp, top = 25.dp)
+                    .fillMaxWidth()
+            }
+
+            GameBoard(
+                modifier = gameBoardModifier,
+                numberOfSquaresInHeight = 21,
+                numberOfSquaresInWidth = 12,
+                snakePixelPositions = snakePixelsPosition
+            )
+
+            val controlSectionModifier = remember {
+                Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(2 / 3f)
+            }
+
+            ControlSection(modifier = controlSectionModifier, onEvent = viewModel::onEvent)
+        }
     }
 }
