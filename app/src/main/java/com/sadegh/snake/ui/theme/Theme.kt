5691feeee1d8b4ce08e8d30354d5a03185026a94
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 03:38:37 +0330

    specify theme colors

--- app/src/main/java/com/sadegh/snake/ui/theme/Theme.kt
+++ app/src/main/java/com/sadegh/snake/ui/theme/Theme.kt
@@ -22,9 +22,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Green,
+    secondary = LightGreen,
+    surface = DarkGreen
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
