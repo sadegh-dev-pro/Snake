8206788efc278a65b2f7dc04b51258cd40f1f369
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 03:46:32 +0330

    set background color of game screen

--- app/src/main/java/com/sadegh/snake/presentation/game_screen/MainScreen.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/MainScreen.kt
@@ -1,8 +1,15 @@
 package com.sadegh.snake.presentation.game_screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun MainScreen() {
+
+    Surface(modifier= Modifier.fillMaxSize()) {
+
+    }
 
 }
