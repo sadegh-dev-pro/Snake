8a7681a9077810d13dce12508299e88005d16de8
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 05:17:56 +0330

    add snakePixelPositions parameter to GameBoard function to draw snake pixels in board

--- app/src/main/java/com/sadegh/snake/presentation/game_screen/components/GameBoard.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/components/GameBoard.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.sadegh.snake.presentation.game_screen.components.util.GameBoardColors
 import com.sadegh.snake.presentation.game_screen.components.util.GameBoardDefaults
@@ -13,12 +14,17 @@ import com.sadegh.snake.presentation.game_screen.components.util.GameBoardDefaul
 @Composable
 fun GameBoard(
     modifier: Modifier,
-    colors: GameBoardColors=GameBoardDefaults.colors(),
+    colors: GameBoardColors = GameBoardDefaults.colors(),
     numberOfSquaresInHeight: Int,
     numberOfSquaresInWidth: Int,
+    snakePixelPositions: List<Pair<Int, Int>>
 ) {
 
     val colorList = remember { listOf(colors.secondColor, colors.firstColor) }
+
+    val snakePixelRowIndices = remember { snakePixelPositions.map { it.first } }
+
+    val snakePixelColumnIndices = remember { snakePixelPositions.map { it.second } }
 
     Canvas(modifier = modifier) {
 
@@ -28,7 +34,16 @@ fun GameBoard(
 
             repeat(numberOfSquaresInWidth) { columnIndex ->
 
-                val squareColor = colorList[(rowIndex + columnIndex) % 2]
+                val squareColor =
+                    if (
+                        rowIndex in snakePixelRowIndices &&
+                        columnIndex in snakePixelColumnIndices
+                    ) {
+                        Color.Blue
+                    } else {
+                        colorList[(rowIndex + columnIndex) % 2]
+
+                    }
 
                 val squareTopLeftOffset = Offset(
                     x = columnIndex * squareSide,
