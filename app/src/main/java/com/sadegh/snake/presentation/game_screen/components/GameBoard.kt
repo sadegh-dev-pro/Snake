04678bbef9a8d7821065ed81710bda27a8a34192
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 04:39:25 +0330

    finish build of basic game board

--- app/src/main/java/com/sadegh/snake/presentation/game_screen/components/GameBoard.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/components/GameBoard.kt
@@ -1,11 +1,46 @@
 package com.sadegh.snake.presentation.game_screen.components
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.dp
+import com.sadegh.snake.presentation.game_screen.components.util.GameBoardColors
+import com.sadegh.snake.presentation.game_screen.components.util.GameBoardDefaults
 
 @Composable
 fun GameBoard(
-    modifier: Modifier
+    modifier: Modifier,
+    colors: GameBoardColors=GameBoardDefaults.colors(),
+    numberOfSquaresInHeight: Int,
+    numberOfSquaresInWidth: Int,
 ) {
 
+    val colorList = remember { listOf(colors.secondColor, colors.firstColor) }
+
+    Canvas(modifier = modifier) {
+
+        val squareSide = this.size.width / numberOfSquaresInWidth
+
+        repeat(numberOfSquaresInHeight) { rowIndex ->
+
+            repeat(numberOfSquaresInWidth) { columnIndex ->
+
+                val squareColor = colorList[(rowIndex + columnIndex) % 2]
+
+                val squareTopLeftOffset = Offset(
+                    x = columnIndex * squareSide,
+                    y = rowIndex * squareSide
+                )
+
+                drawRect(
+                    color = squareColor,
+                    topLeft = squareTopLeftOffset,
+                    size = Size(width = squareSide, height = squareSide)
+                )
+            }
+        }
+    }
 }
