26533bf7ac403024c6500ffa33b739e8678a64e6
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 03:58:58 +0330

    change colors name in GameBoardColors class

--- app/src/main/java/com/sadegh/snake/presentation/game_screen/components/util/GameBoardColors.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/components/util/GameBoardColors.kt
@@ -3,6 +3,6 @@ package com.sadegh.snake.presentation.game_screen.components.util
 import androidx.compose.ui.graphics.Color
 
 data class GameBoardColors(
-    val evenSquareColor:Color,
-    val oddSquareColor:Color
+    val firstColor: Color,
+    val secondColor:Color
 )
