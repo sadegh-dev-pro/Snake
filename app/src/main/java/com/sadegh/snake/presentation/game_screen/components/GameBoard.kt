afc11cf69bc1a7ab2f2381cea6992223ec78522d
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 03:49:10 +0330

    create gameBoard composable function

---
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/components/GameBoard.kt
@@ -0,0 +1,11 @@
+package com.sadegh.snake.presentation.game_screen.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GameBoard(
+    modifier: Modifier
+) {
+
+}
