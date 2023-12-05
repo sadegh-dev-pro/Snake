763da68d9f1932621ecacae6512bedce91f0fb51
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 03:53:46 +0330

    create util class for game board colors

---
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/components/util/GameBoardColors.kt
@@ -0,0 +1,8 @@
+package com.sadegh.snake.presentation.game_screen.components.util
+
+import androidx.compose.ui.graphics.Color
+
+data class GameBoardColors(
+    val evenSquareColor:Color,
+    val oddSquareColor:Color
+)
