56f8b1c9d64a2b31d1bf87fe35eb5b318c039e79
Sadegh <m.sadegh.amerehi@gmail.com>
2023-12-05 05:35:49 +0330

    call GameBoard function in MainScreen function and MainScreen function in Main Activity

--- app/src/main/java/com/sadegh/snake/presentation/game_screen/MainActivity.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SnakeTheme {
+                MainScreen()
             }
         }
     }
--- app/src/main/java/com/sadegh/snake/presentation/game_screen/MainScreen.kt
+++ app/src/main/java/com/sadegh/snake/presentation/game_screen/MainScreen.kt
@@ -1,15 +1,34 @@
 package com.sadegh.snake.presentation.game_screen
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sadegh.snake.presentation.game_screen.components.GameBoard
 
 @Composable
 fun MainScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
 
-    Surface(modifier= Modifier.fillMaxSize()) {
+        val viewModel = viewModel<GameScreenViewModel>()
 
+        val snakePixelsPosition by viewModel.snakePixelPositions.collectAsState()
+
+        GameBoard(
+            modifier = Modifier
+                .padding(start = 25.dp, end = 25.dp, top = 25.dp),
+            numberOfSquaresInHeight =21,
+            numberOfSquaresInWidth =12,
+            snakePixelPositions =snakePixelsPosition
+        )
     }
 
 }
